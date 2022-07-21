a3fcabe56ed399c68b44dee33102e31187218420
Viranson <viransonland@gmail.com>
2022-07-22 00:26:10 +0100

    Add s3 Backend to store tfstate for Remote management

---
+++ tp5/ec2.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAV4P6D24Q7SNPVQYK"
+  secret_key = "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"
+}
+
+terraform {
+  backend "s3" {
+    bucket     = "terraform-backend-etraining"
+    key        = "terraform.tfstate" # Nom du fichier tfstate à stocker dans le compartiment s3
+    region     = "us-east-1"
+    access_key = "AKIAV4P6D24Q7SNPVQYK"
+    secret_key = "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"
+  }
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http_https" {
+  name        = "allow_ssh_http_https"
+  description = "Allow SSH, HTTP and HTTPS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "ALL to VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http_https"
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami             = data.aws_ami.app_ami.id
+  instance_type   = var.instancetype
+  key_name        = "devops-terraform"
+  tags            = var.aws_common_tag
+  security_groups = ["${aws_security_group.allow_ssh_http_https.id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install -y nginx1.12",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("./terraform.pem")
+      host        = self.public_ip
+    }
+  }
+
+  # Pour forcer la suppression des blocs ebs créés pour les instances
+  root_block_device {
+    delete_on_termination = true
+  }
+}
+
+resource "aws_eip" "myec2_eip" {
+  instance = "$(aws_instance.myec2.id)"
+  vpc      = true
+
+  provisioner "local-exec" {
+    command = "echo PUBLIC_IP: ${aws_eip.myec2_eip.public_ip} ; ID: ${aws_instance.myec2.id} ; AZ: ${aws_instance.myec2.availability_zone}; > infos_ec2.txt"
+  }
+}
